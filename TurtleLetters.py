15a0dbb5af2297e9f12188bb6e406eb4d9567310
sarizha <47358514+sarizha@users.noreply.github.com>
2019-02-06 10:24:55 -0500

    Update TurtleLetters.py
    i drew a b that looks like 8

--- TurtleLetters.py
+++ TurtleLetters.py
@@ -37,7 +37,34 @@ def turtleLetter(letter,tur):
         tur.fd(35)
         #tur.right(180)
     elif letter == "B":
-	    pass
+	tur.setheading(0)
+        tur.pu()
+        tur.fd(5)
+        tur.right(90)
+        tur.fd(5)
+        tur.pd()
+        tur.fd(30)
+        tur.right(180)
+        tur.fd(30)
+        tur.right(90)
+        tur.fd(20)
+        tur.right(90)
+        tur.fd(30)
+        tur.right(180)
+        tur.fd(15)
+        tur.left(90)
+        tur.fd(20)
+        tur.left(90)
+        tur.fd(15)
+        tur.left(90)
+        tur.fd(20)
+        tur.pu()
+        #fixes
+        tur.left(90)
+        tur.fd(35)
+        tur.right(90)
+        tur.fd(15)
+        #tur.right(180)
     elif letter == "C":
 	    pass
     elif letter == "D":
